7dc803382d267cf88d34a5e45b79c8b645e75b58
adiohack <adiohack@gmail.com>
2022-06-14 18:27:42 +0300

    finished the map

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar } from "./Navbar/Navbar";
+import { Map } from "../Map/Map";
+import "../Home/style.css";
 
 export function Home() {
   return (
     <>
       <Navbar />
       <h1>Welcome To Weather App</h1>
+      <Map />
     </>
   );
 }
